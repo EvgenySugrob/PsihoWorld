101c32d8646065f3e4ec861d8bde438e82ef9348
EvgenySugrob <xgenstudioz0@gmail.com>
2023-06-25 15:25:49 +0500

    Health and armor
    Расчет здоровья и брони при подборе. Но есть баг, что при подборе здоровья после брони, здоровье будет прибавляться к нулевому значению.

--- Source/PsihoWorld/RetroFPSAttributeSet.cpp
+++ Source/PsihoWorld/RetroFPSAttributeSet.cpp
@@ -7,3 +7,48 @@ URetroFPSAttributeSet::URetroFPSAttributeSet()
 {
 
 }
+
+void URetroFPSAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+	NewValue = FMath::Clamp<float>(NewValue, 0, 100);
+}
+
+bool URetroFPSAttributeSet::PreGameplayEffectExecute(FGameplayEffectModCallbackData& Data)
+{
+	float AbsoluteMagnitude = FMath::Abs(Data.EvaluatedData.Magnitude);
+
+	if (Data.EvaluatedData.Magnitude > 0)
+	{
+
+		if (Data.EvaluatedData.Attribute == GetArmorAttribute() && GetArmor() + AbsoluteMagnitude >= 100)
+		{
+			SetArmor(100);
+			return false;
+		}
+
+		if (Data.EvaluatedData.Attribute == GetHealthAttribute() && GetHealth() + AbsoluteMagnitude >= 100)
+		{
+			SetHealth(100);
+			return false;
+		}
+
+		//if (Data.EvaluatedData.Attribute == GetHealthAttribute() && GetArmor() <= 0)  //Необходимо пофиксить расчет здоровья
+		//{																				//Оставив как есть, то при подборе здоровья после урона, здоровье прибаляется к тому значению, которое было изначально
+		//	Health.SetCurrentValue(Health.GetCurrentValue() + AbsoluteMagnitude);		//Если добавить эту строку, после подбора брони, здоровье прибавляется к нулевому значению
+		//	return false;
+		//}
+
+		if (Data.EvaluatedData.Attribute == GetArmorAttribute() && GetArmor() <= 0)
+		{
+			SetArmor(0);
+
+		}
+	}
+	else
+	{
+		if (Data.EvaluatedData.Attribute == GetArmorAttribute() && AbsoluteMagnitude > GetArmor() && Data.EvaluatedData.Magnitude < 0)
+			Health.SetCurrentValue(Health.GetCurrentValue() - (AbsoluteMagnitude - GetArmor()));
+	}
+
+	return true;
+}
